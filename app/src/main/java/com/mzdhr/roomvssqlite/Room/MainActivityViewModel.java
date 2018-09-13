b3990595c2a2e23d7969a8f9decea57ea4a2b148
MohammadL <mmlaif@gmail.com>
2018-09-13 14:03:14 +0300

    fix: viewmodel

--- app/src/main/java/com/mzdhr/roomvssqlite/Room/MainActivityViewModel.java
+++ app/src/main/java/com/mzdhr/roomvssqlite/Room/MainActivityViewModel.java
@@ -46,7 +46,6 @@ public class MainActivityViewModel extends AndroidViewModel {
             @Override
             public void run() {
                 mNoteDao.deleteNote(noteEntity);
-
             }
         });
     }
